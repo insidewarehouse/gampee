50785fa81225649d9237d0599a71d91930b9510b
Dominykas Blyžė <hello@dominykas.com>
2015-03-20 09:39:51 +0200

    first draft implementation

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-function gampee()
-{
-	throw new Error("Not implemented");
+function gampee(ecData) {
+
+	if (!ecData) {
+		throw new TypeError();
+	}
+
+	if (!Array.isArray(ecData)) {
+		ecData = [ecData];
+	}
+
+	var ecParams = {};
+
+	ecData.forEach(function (dataItem, diIdx) {
+		var dataKeyPrefix = "il" + diIdx;
+
+		ecParams[dataKeyPrefix + "nm"] = dataItem.list;
+		dataItem.products.forEach(function (productItem, piIdx) {
+			var productKeyPrefix = dataKeyPrefix + "pi" + piIdx;
+
+			ecParams[productKeyPrefix + "id"] = productItem.id;
+			ecParams[productKeyPrefix + "nm"] = productItem.name;
+			ecParams[productKeyPrefix + "ps"] = productItem.position;
+
+		});
+	});
+
+	return ecParams;
 }
 
 module.exports = gampee;
--- test/index.js
+++ test/index.js
@@ -5,9 +5,80 @@ var expect = require("chai").expect,
 
 describe("gampee", function () {
 
-	it("should throw 'Not implemented'", function () {
+	it("should throw when called without params", function () {
 
-		expect(gampee).to.throw("Not implemented");
+		expect(gampee).to.throw(TypeError);
+
+	});
+
+	describe("type=impression", function () {
+
+		it("should convert one impression", function () {
+			var ecommerceParams = gampee({
+				"type": "impression",
+				"list": "search",
+				"products": [
+					{"id": "shirtM", "name": "Nice T-Shirt (M)", "position": 1},
+					{"id": "shirtXL", "name": "Nice T-Shirt (XL)", "position": 2}
+				]
+			});
+
+			expect(ecommerceParams).to.eql({
+
+				"il0nm": "search",
+
+				"il0pi0id": "shirtM",
+				"il0pi0nm": "Nice T-Shirt (M)",
+				"il0pi0ps": 1,
+
+				"il0pi1id": "shirtXL",
+				"il0pi1nm": "Nice T-Shirt (XL)",
+				"il0pi1ps": 2
+
+			});
+
+		});
+
+		it("should convert multiple impressions", function () {
+			var ecommerceParams = gampee([
+				{
+					"type": "impression",
+					"list": "search",
+					"products": [
+						{"id": "shirtM", "name": "Nice T-Shirt (M)", "position": 1},
+						{"id": "shirtXL", "name": "Nice T-Shirt (XL)", "position": 2}
+					]
+				},
+				{
+					"type": "impression",
+					"list": "promo",
+					"products": [
+						{"id": "pants", "name": "Baggy Pants", "position": 1}
+					]
+				}
+			]);
+
+			expect(ecommerceParams).to.eql({
+
+				"il0nm": "search",
+
+				"il0pi0id": "shirtM",
+				"il0pi0nm": "Nice T-Shirt (M)",
+				"il0pi0ps": 1,
+
+				"il0pi1id": "shirtXL",
+				"il0pi1nm": "Nice T-Shirt (XL)",
+				"il0pi1ps": 2,
+
+				"il1nm": "promo",
+
+				"il1pi0id": "pants",
+				"il1pi0nm": "Baggy Pants",
+				"il1pi0ps": 1
+
+			});
+
+		});
 
 	});
 
